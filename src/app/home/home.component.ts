6fb9d2926eeecb38f512d10663138b1da3dfbb94
Mostafa-YMS <mostafa_yms@yahoo.com>
2022-01-28 03:25:10 +0200

    Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../movies.service';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  
+  trendingMovies: any[] = [];
+  trendingShows: any[] = [];
+  imageBase: string = 'https://image.tmdb.org/t/p/w500/';
 
-  constructor(private _MoviesService:MoviesService) { }
+  constructor(private _MoviesService: MoviesService) {}
 
-  
   ngOnInit(): void {
-    this._MoviesService.getTrending('Movies').subscribe((data)=>{
-      console.log(data);
-      
-    })
-  }
+    this._MoviesService.getTrending('movies').subscribe((data) => {
+      data.results.sort((a:any,b:any)=>{return b.vote_average - a.vote_average })
+      this.trendingMovies = data.results.slice(0,10);
+    });
 
+    this._MoviesService.getTrending('tv').subscribe((data) => {
+      data.results.sort((a:any,b:any)=>{return b.vote_average - a.vote_average })
+      this.trendingShows = data.results.slice(0,10);
+    });
+  }
 }
